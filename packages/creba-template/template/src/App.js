fbb0cd02ea0770cce3154d8139d89391ce66de4b
Emmanuel Paul Elom <scansioquielom@gmail.com>
2024-06-25 00:11:05 +0100

    fix
    Signed-off-by: Emmanuel Paul Elom <scansioquielom@gmail.com>

--- packages/creba-template/template/src/App.js
+++ packages/creba-template/template/src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Reblend, { useState } from 'reblendjs';
+import Alert from './Alert';
 
 function App() {
   const [i, setState] = useState(0);
   setInterval(() => {
     setState(i + 1);
   }, 1000);
+  const [showAlert, setShowAlert] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Alert show={showAlert}>Happy Reblend</Alert>
+        <p
+          onmouseover={() => setShowAlert(true)}
+          onmouseout={() => setShowAlert(false)}
+        >
           Edit <code>src/App.js</code> and save to reload. {i}
         </p>
         <a
