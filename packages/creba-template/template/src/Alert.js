79fe292b7cf331019784c7bb8e87cac7d6df08fc
Emmanuel Paul Elom <scansioquielom@gmail.com>
2024-06-28 02:09:33 +0100

    fix
    Signed-off-by: Emmanuel Paul Elom <scansioquielom@gmail.com>

--- packages/creba-template/template/src/Alert.js
+++ packages/creba-template/template/src/Alert.js
@@ -1,15 +1,21 @@
 import Reblend from 'reblendjs';
+import { useRef } from 'reblendjs/dist/internal/hooks';
 
 function Alert({ show, children }) {
+  const ref = useRef();
   function onmousemove(e) {
-    this.style.top = e.offsetY + 'px';
-    this.style.left = e.offsetX + 'px';
+    if (ref.current) {
+      ref.current.style.display = 'fixed';
+      ref.current.style.top = e.offsetY + 'px';
+      ref.current.style.left = e.offsetX + 'px';
+    }
   }
 
   return (
     <>
       <h1
-        style={`position: absolute; top: 2px; width: 100%; background: azure; display: ${
+        ref={ref}
+        style={`position: fixed; top: 2px; left: 0; width: 100%; background: azure; display: ${
           show ? 'block' : 'none'
         }`}
       >
